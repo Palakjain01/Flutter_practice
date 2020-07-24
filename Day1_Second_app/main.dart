ea8f1ba6182845071bca0d76f9e9b1fff91636f5
Palakjain01 <60611126+Palakjain01@users.noreply.github.com>
2020-07-24 16:47:47 +0530

    main.dart-c1

---
+++ Day1_Second_app/main.dart
@@ -0,0 +1,14 @@
+import 'ui/home.dart';
+import 'package:flutter/material.dart';
+
+import 'ui/profile.dart';
+
+main()
+{  
+
+// For Easier switching between the two!
+
+
+  //runApp(myapp());
+  runApp(myprofile());
+}
