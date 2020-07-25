21cb45a1dae4bdeff0c0fff689a4af12f3dc1620
Palakjain01 <60611126+Palakjain01@users.noreply.github.com>
2020-07-25 13:47:55 +0530

    main.dart-c1

---
+++ Hot_app/main.dart
@@ -0,0 +1,18 @@
+// Introducing the Concept of Hot Reload!
+
+
+import 'package:flutter/material.dart';
+
+import 'ui/home.dart';
+
+main()
+{
+  runApp(MyHot());
+}
+
+class MyHot extends StatelessWidget{
+  build(BuildContext context)
+  {
+    return MyApp();
+  }
+}
